ba2f8f3762de00028126b3f87b50c122c1223b68
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-09-19 17:22:51 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
+import './App.css';
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
-
-/*
- * dynamic properties is : idealFacingMode, idealResolution, isMaxResolution
- * In this example we set the idealFacingMode
- */
 class App extends Component {
-  constructor (props, context) {
-    super(props, context);
-    this.state = {
-      idealFacingMode: FACING_MODES.ENVIRONMENT
-    };
-    this.renderButtons = this.renderButtons.bind(this);
+  onTakePhoto (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
   }
-
-  renderButtons () {
-    return (
-      <div>
-        <button onClick={ (e) => {
-          this.setState({idealFacingMode: FACING_MODES.USER});
-        }}> FACING_MODES.USER </button>
-
-        <button onClick={ (e) => {
-          this.setState({idealFacingMode: FACING_MODES.ENVIRONMENT});
-        }}> FACING_MODES.ENVIRONMENT </button>
-      </div>
-    );
+ 
+  onCameraError (error) {
+    console.error('onCameraError', error);
   }
-
+ 
+  onCameraStart (stream) {
+    console.log('onCameraStart');
+  }
+ 
+  onCameraStop () {
+    console.log('onCameraStop');
+  }
+ 
   render () {
     return (
       <div className="App">
-        { this.renderButtons() }
         <Camera
-          idealFacingMode = {this.state.idealFacingMode}
-          onTakePhoto = { () => {} }
+          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+          onCameraError = { (error) => { this.onCameraError(error); } }
+          idealFacingMode = {FACING_MODES.ENVIRONMENT}
+          idealResolution = {{width: 640, height: 480}}
+          imageType = {IMAGE_TYPES.JPG}
+          imageCompression = {0.97}
+          isMaxResolution = {false}
+          isImageMirror = {false}
+          isSilentMode = {true}
+          isDisplayStartCameraError = {true}
+          isFullscreen = {true}
+          sizeFactor = {1}
+          onCameraStart = { (stream) => { this.onCameraStart(stream); } }
+          onCameraStop = { () => { this.onCameraStop(); } }
         />
       </div>
     );
   }
 }
-
+ 
 export default App;
