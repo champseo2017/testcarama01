4037d6e6bc104ce683afcc451d99b190fddd33ed
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-09-19 17:44:48 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import Webcam from "react-webcam";
 
 function App() {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
   const webcamRef = React.useRef(null);
  
   const capture = React.useCallback(
